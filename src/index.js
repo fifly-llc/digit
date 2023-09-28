7b7d19a5205ae75a4c7a62897ff93c645083210a
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-09-28 18:10:20 +1000

    fixxy

--- src/index.js
+++ src/index.js
@@ -10,12 +10,26 @@ let messageArray = [];
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send(fs.readFileSync('./index.html'));
+app.get("/", (req, res) => {
+    fs.readFile('./index.html', 'utf8', (error, data) => {
+        if (error) {
+            console.error('Error reading file:', error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send(data);
+        }
+    });
 });
 
-app.get('/app', (req, res) => {
-    res.send(fs.readFileSync('./app/index.html'));
+app.get("/app", (req, res) => {
+    fs.readFile('./app/index.html', 'utf8', (error, data) => {
+        if (error) {
+            console.error('Error reading file:', error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send(data);
+        }
+    });
 });
 
 app.post('/api', (req, res) => {
