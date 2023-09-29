1225c2307ae617d287e2586f292d6ec25b889d09
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-09-29 19:56:56 +1000

    add constant new message checks

--- index.js
+++ index.js
@@ -9,18 +9,12 @@ let messageArray = [];
 let threads = [];
 
 function sanitize(inputString) {
-    var nonFormattingTagsRegex = /<(?!\/?(a|b|i|u|em|strong|span|abbr|acronym|address|bdo|big|cite|code|del|dfn|font|kbd|ins|mark|pre|q|rp|rt|ruby|s|samp|small|strike|sub|sup|tt|var|bdi|bgsound|blink|spacer)[\s\/>])([a-zA-Z-]+)(?:\s[^>]*)?>|<\/(?!a|b|i|u|em|strong|span|abbr|acronym|address|bdo|big|cite|code|del|dfn|font|kbd|ins|mark|pre|q|rp|rt|ruby|s|samp|small|strike|sub|sup|tt|var|bdi|bgsound|blink|spacer)[\s>])/gi;
-    var outputString = inputString.replace(nonFormattingTagsRegex, '');
-    return outputString.replace(/\s+/g, ' ').replace('&nbsp;', '').replace('\n', '');
-}
+    const allowedTags = ['a', 'b', 'i', 'u', 'em', 'strong', 'span', 'abbr', 'acronym', 'address', 'bdo', 'big', 'cite', 'code', 'del', 'dfn', 'font', 'kbd', 'ins', 'mark', 'pre', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'small', 'strike', 'sub', 'sup', 'tt', 'var', 'bdi', 'bgsound', 'blink', 'spacer'];
 
-function genThreadId() {
-    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    for (let i = 0; i < 10; i++) {
-        result += chars[Math.floor(Math.random() * chars.length)];
-    }
-    return result;
+    const nonFormattingTagsRegex = new RegExp(`<(?!\/?(${allowedTags.join('|')}))([a-zA-Z-]+)(?:\s[^>]*)?>|<\/(?!${allowedTags.join('|')})[\s>])`, 'gi');
+    const outputString = inputString.replace(nonFormattingTagsRegex, '');
+
+    return outputString.replace(/\s+/g, ' ').replace('&nbsp;', '').replace('\n', '');
 }
 
 app.use(bodyParser.json());
@@ -48,32 +42,27 @@ app.get("/app", (req, res) => {
     });
 });
 
+app.get("/app/thread", (req, res) => {
+    fs.readFile('./app/thread/index.html', 'utf8', (error, data) => {
+        if (error) {
+            console.error('Error reading file:', error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send(data);
+        }
+    });
+});
+
 app.post('/api', (req, res) => {
     let body = req.body;
 
     if(body.type === 'getMessages') {
         res.send({ messages: messageArray });
     } else if (body.type === 'postMessage') {
-        messageArray.push(sanitize(body.message));
+        messageArray.push({ content: sanitize(body.message), username: sanitize(body.username), timestamp: sanitize(body.timestamp) });
         res.sendStatus(200);
-    } else if (body.type === 'getThreadMessages') {
-        res.send({ messages: threads[body.threadId].messages });
-    } else if (body.type === 'postThreadMessage') {
-        if (body.password === threads[body.threadId].password)
-            res.send(find(item => item.id === body.threadId).messages);
-        else
-            res.sendStatus(401);
-    } else if (body.type === 'createThread') {
-        if (body.visability === 'private') {
-            threads.push({ messages: [], visability: false, password: body.password, id: genThreadId() });
-            res.sendStatus(200);
-        } else {
-            threads.push({ messages: [], visability: true, id: genThreadId() });
-            res.sendStatus(200);
-        }
-    } else if (body.type === 'deleteThread') {
-        threads = threads.filter(item => item.id !== body.threadId);
-        res.sendStatus(200);
+    } else {
+        res.sendStatus(400);
     }
 });
 
