238bf0ab90aa8b114c20b125c8cbf32d9f6c6a65
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-10-02 18:39:53 +1100

    add voice chat v0.1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 let messageArray = [];
-let threads = [];
+let voiceChats = [];
 
 function sanitize(inputString) {
     const allowedTags = ['a', 'b', 'i', 'u', 'em', 'strong', 'span', 'abbr', 'acronym', 'address', 'bdo', 'big', 'cite', 'code', 'del', 'dfn', 'font', 'kbd', 'ins', 'mark', 'pre', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'small', 'strike', 'sub', 'sup', 'tt', 'var', 'bdi', 'bgsound', 'blink', 'spacer'];
@@ -42,25 +42,25 @@ app.get("/app", (req, res) => {
     });
 });
 
-app.get("/app/thread", (req, res) => {
-    fs.readFile('./app/thread/index.html', 'utf8', (error, data) => {
-        if (error) {
-            console.error('Error reading file:', error);
-            res.status(500).send('Internal Server Error');
-        } else {
-            res.send(data);
-        }
-    });
-});
-
 app.post('/api', (req, res) => {
     let body = req.body;
 
-    if(body.type === 'getMessages') {
+    if (body.type === 'getMessages') {
         res.send({ messages: messageArray });
     } else if (body.type === 'postMessage') {
         messageArray.push({ content: sanitize(body.message.content), username: sanitize(body.message.username), timestamp: sanitize(body.message.timestamp) });
         res.sendStatus(200);
+    } else if (body.type === 'postVoice') {
+        let vc = voiceChats.find(vc => vc.code === body.code);
+        if (vc) {
+            vc.audio = body.audio;
+            res.sendStatus(200);
+        }
+    } else if (body.type === 'getVoice') {
+        let vc = voiceChats.find(vc => vc.code === body.code);
+        if (vc) {
+            res.send(vc.audio);
+        }
     } else {
         res.sendStatus(400);
     }
