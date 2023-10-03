15dcd13aa70d379b33602c0e87bc39eb08ee77c7
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-10-03 17:20:51 +1100

    fix bot styling

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/api', (req, res) => {
         messageArray.push({ content: body.message.content, username: body.message.username, timestamp: body.message.timestamp });
         res.sendStatus(200);
     } else if (body.type === 'botMessage') {
-        messageArray.push({ content: '<b><i style="color: lightblue; font-weight: bold; font-size: 16px;">' + body.message.content + '</i></b>', username: '[BOT] ' + body.message.username, timestamp: 'Automated Message' });
+        messageArray.push({ content: '<i style="color: black; font-weight: bold; font-size: 16px;">' + body.message.content + '</i>', username: '[BOT] ' + body.message.username, timestamp: 'Automated Message' });
         res.sendStatus(200);
     } else {
         res.sendStatus(400);
