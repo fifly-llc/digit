a61e38340018d2c9a14dc604fad1445d19650689
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-10-21 15:33:37 +1100

    supa big upgrade

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ const port = 3000;
 
 let messageArray = [];
 
+//let adminAuth = genRandom(5);
+let adminAuth = "441a"; // Set this to whatever or use random generation
+
+console.log('[NOTICE] Authentication for Admin Portal is <' + adminAuth + '>.');
+
+/*function genRandom(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}*/
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -38,14 +53,27 @@ app.post('/api', (req, res) => {
     if (body.type === 'getMessages') {
         res.send({ messages: messageArray });
     } else if (body.type === 'postMessage') {
+        if (body.message.username.startsWith('[ADMIN]') || body.message.username.startsWith('[BOT]') || body.message.username.startsWith('[SYSTEM]')) {
+            body.message.username = "I pretended to be an admin, a bot, or a system bot (" + body.message.username + ")";          
+        }
+
         messageArray.push({ content: body.message.content, username: body.message.username, timestamp: body.message.timestamp });
         res.sendStatus(200);
     } else if (body.type === 'botMessage') {
         messageArray.push({ content: body.message.content, username: '[BOT] ' + body.message.username, timestamp: 'BOT MESSAGE' });
         res.sendStatus(200);
     } else if (body.type === 'systemMessage') {
-        messageArray.push({ content: body.message.content, username: '[SYSTEM] ', timestamp: 'SYSTEM MESSAGE' });
+        messageArray.push({ content: body.message.content, username: '[SYSTEM] ' + body.message.username, timestamp: 'SYSTEM MESSAGE' });
         res.sendStatus(200);
+    } else if (body.type === 'adminMessage') {
+        if (body.auth !== adminAuth) {
+            body.message.username = "I pretended to be an admin (" + body.message.username + ")";
+        }
+
+        messageArray.push({ content: body.message.content, username: '[ADMIN] ' + body.message.username, timestamp: body.message.timestamp });
+        res.sendStatus(200);
+    } else if (body.type === 'getAdminAuth') {
+        res.send({ auth: adminAuth });
     } else {
         res.sendStatus(400);
     }
